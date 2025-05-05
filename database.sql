0a4143ed46c6bd1024794f9093a6b39c972fec15
kimrobbin <kimrobbinkf@gmail.com>
2025-05-05 11:40:10 +0200

    Fiksa basic sql insert

---
+++ database.sql
@@ -0,0 +1,11 @@
+create databases varelager,
+    use varelager;
+
+create table varer ( 
+    id int auto_increment primary key,
+    varenummer int not null,
+    navn varchar(50) not null,
+    pris decimal(10, 2) not null,
+    antall int not null,
+    katogori enum("Elektronikk", "Konor", "Klær") not null
+);
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 from db import *
+
+
+sql_statement = "INSERT INTO varer (varenummer, navn, pris, antall, katogori) VALUES (%s, %s, %s, %s, %s)"
+valus = ("1001", "Kamera", 2000, 30, "elektronikk")
+mycursor.execute(sql_statement, valus)
+
+dbconn.commit()
