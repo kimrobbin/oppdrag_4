0c860863887420b41f6ed1bece0d42a435deae47
kimrobbin <kimrobbinkf@gmail.com>
2025-05-07 11:09:40 +0200

    Fixed variabled for evrything  and fixed ny_varer acually have the right catagories.

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ def print_ui():
     print("5. Slett en vare")
     print("6. Avslutt")
     print("--------------------------------------------------------------")
-    userinput = input("Velg mellom 1-5: ")
+    userinput = input("Velg mellom 1-6: ")
     return userinput
+
+def strek():
+    print("--------------------------------------------------------------")
 
 def ny_vare():
     os.system("cls")
@@ -29,11 +32,11 @@ def ny_vare():
     pris = input("Pris:")
     antall = input("Antall:")
     katagori = input(" A) Elektonikk B) kontor C) Klaer")
-    if katagori == "A" or "a":
+    if katagori == "A" or katagori =="a":
         katagori = "Elektronikk"
-    elif katagori == "B" or "b":
+    elif katagori == "B" or katagori == "b":
         katagori = "Kontor"
-    elif katagori == "C" or "c":
+    elif katagori == "C" or katagori == "c":
         katagori = "Klaer"
     
     sql_statement = "INSERT INTO varer (varenummer, navn, pris, antall, katogori) VALUES (%s, %s, %s, %s, %s)"
@@ -43,17 +46,74 @@ def ny_vare():
 
 def alle_varer():
     os.system("cls")
-    sql_statement = "SELECT * FROM varer"
-    mycursor.execute(sql_statement)
-    result = mycursor.fetchall()
-
+    print("LAGERLISTE")
+    strek()
+    valg = input(" Velg mellom: A) Vis alle Varer, B) Elektronikk, C) Kontor, D) Klaer")
+    if valg == "A" or valg == "a":
+        sql_statement = "SELECT * FROM varer"
+        mycursor.execute(sql_statement)
+        result = mycursor.fetchall()
+        
+        if mycursor.rowcount > 0:
+            for row in result:
+                print(row)
+        else:
+            print("Ingen varer funnet")
+            
+            
+                
+        print("Alle varer")
+        
+    elif valg == "B" or valg == "b":
+        sql_statement = "SELECT * FROM varer WHERE katogori='Elektronikk'"
+        mycursor.execute(sql_statement)
+        result = mycursor.fetchall()
+        
+        if mycursor.rowcount > 0:
+            print("Alle varer i Elektronikk")
+            strek()
+            for row in result:
+                print(row)
+            strek()
+        else:
+            print("Ingen varer funnet")
+            
+        
+    elif valg == "C" or valg == "c":
+        sql_statement = "SELECT * FROM varer WHERE katogori='Kontor'"
+        mycursor.execute(sql_statement)
+        result = mycursor.fetchall()
+        
+        if mycursor.rowcount > 0:
+            print("Alle varer i Kontor")
+            strek()
+            for row in result:
+                print(row)
+            strek()
+        else:
+            print("Ingen varer funnet")
+            
+            
+            
+        
+    elif valg == "D" or valg == "d":
+        sql_statement = "SELECT * FROM VARER WHERE katogori='Klaer'"
+        mycursor.execute(sql_statement)
+        result = mycursor.fetchall()
+        
+        if mycursor.rowcount > 0:
+            strek()
+            print("Alle varer i Klaer")
+            strek()
+            for row in result:
+                print(row)
+            strek()
+        else:
+            print("Ingen varer funnet")
     
-    for row in result:
-        print(row)
-    2
-    Continue = input("Press enter to continue")
-    if Continue == "":
-        print_ui()   
+    
+    input("Press enter to continue")
+     
 
 def finn_en_vare():
     os.system("cls")
@@ -64,60 +124,68 @@ def finn_en_vare():
     if result:
         for row in result:
             print(row)
+        strek()
     else:
         print("Ingen varer funnet med det varenummeret.")
     
-    Continue = input("Press enter to continue")
-    if Continue == "":
-        print_ui()
+    input("Press enter to continue")
+    
 
-def finn_Oppdater_en_vare():
+def Oppdater_en_vare():
     os.system("cls")
     varenummer = input("Varenummer:")
     navn = input("Navn:")
     pris = input("Pris:")
     antall = input("Antall:")
-    katagori = input(" A) Elektonikk B) kontor C) Klaer")
-    if katagori == "A" or "a":
-        katagori = "Elektronikk"
-    elif katagori == "B" or "b":
-        katagori = "Kontor"
-    elif katagori == "C" or "c":
-        katagori = "Klaer"
     
-    sql_statement = "UPDATE varer SET navn=%s, pris=%s, antall=%s, katogori=%s WHERE varenummer=%s"
-    mycursor.execute(sql_statement, (navn, pris, antall, katagori, varenummer))
+    
+    sql_statement = "UPDATE varer SET navn=%s, pris=%s, antall=%s,  WHERE varenummer=%s"
+    mycursor.execute(sql_statement, (navn, pris, antall, varenummer))
     dbconn.commit()
     
+    input("Press enter to continue")
+  
+def slett_vare():
+    os.system("cls")
+    varenummer = input("Varenummer:")
+    sql_statement = "DELETE FROM varer WHERE varenummer=%s"
+    mycursor.execute(sql_statement, (varenummer,))
+    dbconn.commit()
+    if mycursor.rowcount > 0:
+        print("Vare slettet")
+    else:
+        print("Ingen varer funnet med det varenummeret.")
     
-    Continue = input("Press enter to continue")
-    if Continue == "":
-        print_ui()
+    input("Press enter to continue")
 
-userinput = print_ui()
 
 
 
-if userinput == "1":
+while True:
+    userinput = print_ui()
     
-    ny_vare()
-    
-    
-elif userinput == "2":
-    
-    alle_varer()
-    
+    if userinput == "1":
+        ny_vare()
         
-elif userinput == "3":
-    finn_en_vare()
         
+    elif userinput == "2":
+        alle_varer()
         
-elif userinput == "4":
-    finn_Oppdater_en_vare()
-    
+            
+    elif userinput == "3":
+        finn_en_vare()
+            
+            
+    elif userinput == "4":
+        Oppdater_en_vare()
+        
+    elif userinput == "5":
+        slett_vare()
 
-elif userinput == "6":
-    print("Avslutter")
-    quit()
-    
+
+    elif userinput == "6":
+        print("Avslutter")
+        quit()
+    else: 
+        print(" Ugyldig valg")
 
