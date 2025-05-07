58553cb7586222132a9d8e9f52aa9d89e43a61c6
kimrobbin <kimrobbinkf@gmail.com>
2025-05-07 08:49:35 +0200

    Made exit and  updated 3

--- main.py
+++ main.py
@@ -17,33 +17,62 @@ def print_ui():
     print("5. Avslutt")
     print("--------------------------------------------------------------")
 
-print_ui()
 
-userinput = input("Velg mellom 1-5: ")
 
-if userinput == "1":
-    varenummer = input("Varenummer:")
-    navn = input("Navn:")
-    pris = input("Pris:")
-    antall = input("Antall:")
-    katagori = input(" A) Elektonikk B) kontor C) Klaer")
-    if katagori == "A" or "a":
-        katagori = "Elektronikk"
-    elif katagori == "B" or "b":
-        katagori = "Kontor"
-    elif katagori == "C" or "c":
-        katagori = "Klaer"
+while True:
+
+    print_ui()
+
+
+    userinput = input("Velg mellom 1-5: ")
+
+    if userinput == "1":
+        varenummer = input("Varenummer:")
+        navn = input("Navn:")
+        pris = input("Pris:")
+        antall = input("Antall:")
+        katagori = input(" A) Elektonikk B) kontor C) Klaer")
+        if katagori == "A" or "a":
+            katagori = "Elektronikk"
+        elif katagori == "B" or "b":
+            katagori = "Kontor"
+        elif katagori == "C" or "c":
+            katagori = "Klaer"
+        
+        sql_statement = "INSERT INTO varer (varenummer, navn, pris, antall, katogori) VALUES (%s, %s, %s, %s, %s)"
+        valus = (varenummer, navn, pris, antall, katagori)
+        mycursor.execute(sql_statement, valus)
+        dbconn.commit()
     
-    sql_statement = "INSERT INTO varer (varenummer, navn, pris, antall, katogori) VALUES (%s, %s, %s, %s, %s)"
-    valus = (varenummer, navn, pris, antall, katagori)
-    mycursor.execute(sql_statement, valus)
-    dbconn.commit()
-   
-    
-if userinput == "2":
-    sql_statement = "SELECT * FROM varer"
-    mycursor.execute(sql_statement)
-    result = mycursor.fetchall()
-    
-    for row in result:
-        print(row)
+        
+    if userinput == "2":
+        sql_statement = "SELECT * FROM varer"
+        mycursor.execute(sql_statement)
+        result = mycursor.fetchall()
+        
+        print("--------------------------------------------------------------")
+        for row in result:
+            print(row)
+            
+            
+    if userinput == "3":
+        varenummer = input("Varenummer:")
+        navn = input("Navn:")
+        pris = input("Pris:")
+        antall = input("Antall:")
+        katagori = input(" A) Elektonikk B) kontor C) Klaer")
+        if katagori == "A" or "a":
+            katagori = "Elektronikk"
+        elif katagori == "B" or "b":
+            katagori = "Kontor"
+        elif katagori == "C" or "c":
+            katagori = "Klaer"
+        
+        sql_statement = "UPDATE varer SET navn=%s, pris=%s, antall=%s, katogori=%s WHERE varenummer=%s"
+        mycursor.execute(sql_statement, (navn, pris, antall, katagori, varenummer))
+        dbconn.commit()
+        
+
+    if userinput == "5":
+        print("Avslutter")
+        quit()
